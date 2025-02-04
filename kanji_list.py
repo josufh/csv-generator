c0305a122feab16f06115a26a9a319230617ab0f
Joshua Ferrer <josuferrer@outlook.com>
2025-02-04 21:48:36 +0900

    漢字N5１週間目２日目入力済み

--- kanji_list.py
+++ kanji_list.py
@@ -127,6 +127,102 @@ kanji_n5 = [
         "examples_kunyomi": "",
         "examples_irregular": ""
     },
+    {
+        "kanji": "何",
+        "onyomi": "",
+        "kunyomi": "なん　なに",
+        "examples_onyomi": "",
+        "examples_kunyomi": "何時　何時間　何　何語",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "時",
+        "onyomi": "ジ",
+        "kunyomi": "",
+        "examples_onyomi": "九時　四時間",
+        "examples_kunyomi": "",
+        "examples_irregular": "時計"
+    },
+    {
+        "kanji": "分",
+        "onyomi": "フン　ブン",
+        "kunyomi": "",
+        "examples_onyomi": "五分　三十分　半分",
+        "examples_kunyomi": "",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "間",
+        "onyomi": "カン",
+        "kunyomi": "あいだ　ま",
+        "examples_onyomi": "時間　一週間",
+        "examples_kunyomi": "ＡとＢの間　この間　間に合う",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "半",
+        "onyomi": "ハン",
+        "kunyomi": "",
+        "examples_onyomi": "七時半　一時間半",
+        "examples_kunyomi": "",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "午",
+        "onyomi": "ゴ",
+        "kunyomi": "",
+        "examples_onyomi": "午後",
+        "examples_kunyomi": "",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "前",
+        "onyomi": "ゼン",
+        "kunyomi": "まえ",
+        "examples_onyomi": "午前",
+        "examples_kunyomi": "前　名前",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "後",
+        "onyomi": "ゴ",
+        "kunyomi": "あと　うし（ろ）",
+        "examples_onyomi": "午後",
+        "examples_kunyomi": "後で　後ろ",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "今",
+        "onyomi": "コン",
+        "kunyomi": "いま",
+        "examples_onyomi": "今月　今週",
+        "examples_kunyomi": "今",
+        "examples_irregular": "今日　今年　今朝"
+    },
+    {
+        "kanji": "週",
+        "onyomi": "シュウ",
+        "kunyomi": "",
+        "examples_onyomi": "先週　来週",
+        "examples_kunyomi": "",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "年",
+        "onyomi": "ネン",
+        "kunyomi": "とし",
+        "examples_onyomi": "一年　去年　来年",
+        "examples_kunyomi": "今年　半年",
+        "examples_irregular": ""
+    },
+    {
+        "kanji": "毎",
+        "onyomi": "マイ",
+        "kunyomi": "",
+        "examples_onyomi": "毎年　毎日　毎月",
+        "examples_kunyomi": "",
+        "examples_irregular": ""
+    },
 ]
 
 kanji_template = [
